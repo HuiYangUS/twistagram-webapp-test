393d1433e0db8e6aee2d3b02da04fd7bb3ae8896
Hui Yang <hui.yang.9045@gmail.com>
2024-03-09 03:35:02 -0500

    test first deployment

--- src/test/java/demo/ui/app/InitialTest.java
+++ src/test/java/demo/ui/app/InitialTest.java
@@ -2,21 +2,25 @@ package demo.ui.app;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import demo.ui.base.web.DriverFactoryWebBase;
 import utilities.ConfigReader;
 
-public class DemoTest extends DriverFactoryWebBase {
+public class InitialTest extends DriverFactoryWebBase {
 
 	private static String url = ConfigReader.getValue("config", "url");
 
 	@Test
 	@DisplayName("Demo Twista Gram App Test")
+	@Tags(value = { @Tag("home"), @Tag("smoke"), @Tag("TWIS-43") })
 	void runTest() {
 		driver.navigate().to(url);
 		String expectedTitle = "Twistagram";
 		assertEquals(expectedTitle, driver.getTitle());
+		webUtils.savesScreenshot("home", true);
 	}
 
 }

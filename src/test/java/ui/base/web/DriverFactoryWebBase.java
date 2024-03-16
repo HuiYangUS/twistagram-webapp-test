3a4b090d9be631c61d7f4e5b9c737ee88857cde9
Hui Yang <hui.yang.9045@gmail.com>
2024-03-15 22:20:38 -0400

    updated TwistaGram Login with Google Test

---
+++ src/test/java/ui/base/web/DriverFactoryWebBase.java
@@ -0,0 +1,39 @@
+package ui.base.web;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import ui.base.config.SimpleReportExtension;
+import ui.base.config.WebTestConfig;
+import utilities.DriverManager;
+import utilities.PageManager;
+import utilities.WebUtils;
+
+@ExtendWith({ WebTestConfig.class, SimpleReportExtension.class })
+public class DriverFactoryWebBase {
+
+	protected static WebDriver driver;
+	protected static WebDriverWait wait;
+	protected static PageManager pages;
+	protected static WebUtils webUtils;
+
+	@BeforeEach
+	protected void setUp() {
+		driver = DriverManager.getDriver();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		pages = PageManager.getInstance();
+		webUtils = new WebUtils(driver);
+	}
+
+	@AfterEach
+	protected void tearDown() {
+		DriverManager.reset();
+		PageManager.reset();
+	}
+
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,9 @@
+@login
+Feature: Login
+
+  @happy @google @smoke
+  Scenario: Log in [Twistagram] app using [Google] credentials
+    Given user navigates to [Twistagram] application
+    When user clicks on [Log in with Google] button
+    And some other action
+    Then user is on the home page

084f1fdcd95ca9bb58a297be7f4406aa4b4f8e6a
Hui Yang <hui.yang.9045@gmail.com>
2024-03-17 08:23:11 -0400

    added FirefoxTest

---
+++ src/test/java/ui/app/FirefoxTest.java
@@ -0,0 +1,48 @@
+package ui.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import ui.base.web.DriverFactoryWebBase;
+import utilities.ConfigReader;
+
+public class FirefoxTest extends DriverFactoryWebBase {
+
+	private static String url = ConfigReader.getValue("config", "url");
+
+	@BeforeAll
+	static void setupHeadlessMode() {
+		System.setProperty("browser", "firefox");
+	}
+
+	@Test
+	@DisplayName("Twista Gram App Google Login Test - Firefox Edition")
+	@Tags(value = { @Tag("main"), @Tag("smoke") })
+	void runTest() {
+		driver.navigate().to(url);
+		String expectedTitle = "Twistagram";
+		assertEquals(expectedTitle, driver.getTitle());
+		try {
+			driver.findElement(By.linkText("Sign out")).click();
+			driver.findElement(By.id("submitButton")).click();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		driver.findElement(By.xpath("//button//*[name()='svg' and @data-testid='GoogleIcon']/ancestor::button"))
+				.click();
+		String emailXpath = String.format("//div[@data-email='%s']/..", ConfigReader.getValue("config", "email"));
+		webUtils.mouse().click(driver.findElement(By.xpath(emailXpath))).perform();
+		driver.findElement(By.xpath("//button//span[text()='Continue']/ancestor::button")).click();
+		String tempXpath = "//main/div/div/span";
+		String expectedHomeMessage = String.format("Logged in as %s", ConfigReader.getValue("config", "fullName"));
+		String actualHomeMessage = driver.findElement(By.xpath(tempXpath)).getText();
+		assertEquals(expectedHomeMessage, actualHomeMessage);
+	}
+
+}
